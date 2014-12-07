93cfa945bc3f4facbd9c65d390d96fa9280c3daa
Vince Cima <contact@vincecima.com>
2014-12-07 12:24:33 -0600

    Update version to retail.1

--- battle.net/tools/chocolateyInstall.ps1
+++ battle.net/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'battle.net'
 $fileType = 'exe'
-$url = 'http://dist.blizzard.com/downloads/bna-installers/322d5bb9ae0318de3d4cde7641c96425/beta.3/Battle.net-Beta-Setup-enUS.exe'
+$url = 'http://dist.blizzard.com/downloads/bna-installers/322d5bb9ae0318de3d4cde7641c96425/retail.1/Battle.net-Setup-enUS.exe'
 $silentArgs = ''
 
 Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
