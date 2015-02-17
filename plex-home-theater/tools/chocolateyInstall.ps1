b1b7f21089abfd09f4503bc4cb001f4808f245ac
Chandler <chandler.newby@gmail.com>
2015-02-17 15:32:51 -0700

    Update PHT to newest version

--- plex-home-theater/tools/chocolateyInstall.ps1
+++ plex-home-theater/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'plex-home-theater'
 $fileType = 'exe'
-$url = 'http://downloads.plexapp.com/plex-home-theater/1.0.9.180-bde1e61d/PlexHomeTheater-1.0.9.180-bde1e61d-windows-x86.exe'
+$url = 'https://downloads.plex.tv/plex-home-theater/1.3.5.431-7966a4df/PlexHomeTheater-1.3.5.431-7966a4df-windows-x86.exe'
 $silentArgs = '/S'
 
 Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
