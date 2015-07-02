959b9e765131ce99a9e58bd5ac25d4571ed82a46
Vince Cima <contact@vincecima.com>
2015-07-01 20:12:24 -0500

    Remove PHT and YNAB, other maintainers have taken over

--- plex-home-theater/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-$packageName = 'plex-home-theater'
-$fileType = 'exe'
-$url = 'https://downloads.plex.tv/plex-home-theater/1.3.5.431-7966a4df/PlexHomeTheater-1.3.5.431-7966a4df-windows-x86.exe'
-$silentArgs = '/S'
-
-Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
